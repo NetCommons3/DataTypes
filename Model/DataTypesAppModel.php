4f625ca90a6ce219c3ed9a1bcc1b7d6f69478245
s-nakajima <nakajimashouhei@gmail.com>
2015-07-24 22:20:28 +0900

    Initial commit

--- Model/DataTypesAppModel.php
+++ Model/DataTypesAppModel.php
@@ -1,7 +1,22 @@
 <?php
+/**
+ * DataTypesApp Model
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
 
 App::uses('AppModel', 'Model');
 
+/**
+ * UserManagerApp Model
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\DataType\Controller
+ */
 class DataTypesAppModel extends AppModel {
 
 }
